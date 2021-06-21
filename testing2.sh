eb884f06755c67e3d39222af598f44ff72e0b05b
fractaljc <joanmanuel.ac@gmail.com>
2021-06-21 22:20:08 +0100

    script

--- testing2.sh
+++ testing2.sh
@@ -2,28 +2,30 @@
 # ...Comment header goes here...
 # scripty testing
 
-set -o errexit 
-
-set -o nounset 
-
+set -o errexit
+set -o nounset
 set -o pipefail
 
-​main() { 
+main() {
+  arg1="${1}"
+  arg2="${2}"
+  files=0
+  specific_files=0
+  directories=0
+  for file in ${arg1}; do
+#    echo "File: ${file}"
+    if [[ -d "${file}" ]]; then
+      (( directories += 1 ))
+    elif [[ -f "${file}" ]]; then
+      (( files += 1 ))
+        if [[ "${file}" =~ $arg2 ]]; then
+          (( specific_files += 1 ))
+        fi
+    fi
+  done
 
-  for file in *; do
-
-  if [[ -d "${file}" ]]; then
-
-    echo "Directory: ${file}"
-
-  elif [[ -f "${file}" ]]; then
-
-    echo "File: ${file}"
-
-  fi
-
-done
-
-} 
-
+  echo "Files: ${files}"
+  echo "${arg2} files: ${specific_files}"
+  echo "Directories: ${directories}"
+}
 main "${@}"
