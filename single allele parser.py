2d3dadbaad362ff1911f56ced8d07b4384f13dd5
fractaljoan <joanmanuel.ac@gmail.com>
2022-05-18 23:14:53 +0100

    class to parse raw outout from netMHCpan tested and working

--- single allele parser.py
+++ single allele parser.py
@@ -1,51 +1,193 @@
 #!/usr/bin/env python3
-#version 1.1
+#version 1.9
 
 import pandas as pd
 import re
 
 
 #*************************************************************************
-#this function will use the raw data from NetMHCpan 4.1 and parse it by the eluted ligand (EL)
-#it returns a csv file with the data parsed for only one allele
+#this class  will use the raw data from NetMHCpan 4.1 and parse it by
+#the eluted ligand (EL)
+#it returns a csv file with the data parsed for at least 4 alleles
+#data = Parser("xxx.xls") call the instance with the output fron netMHCpan
+#data.alleles("xxx.xls")  call to parse the file and save each allele as csv
 
-def allele_parser(filename):
-    """Return a csv file with the parsed data from netMHCpan"""
+class Parser:
+    """
+    explanation.
 
-    df = pd.read_table(filename) #r"" or just "" if it is in the same folder
+    Object attributes:
+    xxx (str): .
+    yyy (str): .
+    zzz (list): .
 
-    f = open(filename) #r""
+    Object methods:
+    __init__(self, output): Create a DataFrame object and
+    opens file to perform regex.
+    """
 
-
-    #netMHCpan gives an output with the allele as first row
-    #name of the allele extracted to be use for the file name
-    line = f.readlines(1)
-    p = re.compile(r'(HLA-\w*):(\w*)')
-    line = str(line)
-    match = p.search(line)
-    allele_name = match.group(1) + match.group(2)
-
-    #dff to be used to create multiples csv files in future versions
-    dff = df.reset_index()
-
-    #here the script selects the row containing the columns name
-    dff.columns = dff.iloc[0]
-    dff = dff.drop(0)
-
-    #data type object changed in order to help to manipulate the data later on
-    dff["Pos"] = dff["Pos"].astype(int)
-    dff["EL_Rank"] = dff["EL_Rank"].astype(float)
-    dff["BA_Rank"] = dff["BA_Rank"].astype(float)
+    def __init__(self, netMHCpan_output):
+        self.df = pd.read_table(netMHCpan_output, low_memory=False)
 
 
-    #columns of relevance selected and threshold recommended by NetMHCpan-4.1 developers
-    dff = dff[["Pos", "Peptide", "ID", "EL_Rank", "BA_Rank"]].query("EL_Rank <= 2")
-    dff = dff.sort_values(by=["EL_Rank"])
+    def alleles(self, netMHCpan_output):
 
-    #saves the parsed data sorted by EL Rank% only at this moment
-    dff = dff.reset_index(drop=True)
+        self.f = open(netMHCpan_output)
+        line = self.f.readlines(1)
+        p = re.compile(r'(HLA-\w*):(\w*)')
+        line = str(line)
+        it = p.finditer(line)
+        alleles_list = []
 
-    return dff.to_csv(f"{allele_name}.csv", index=False)
+        for match in it:
+            alleles_list.append(match.group(1) + match.group(2))
+
+
+        # common columns
+        df_common = self.df.iloc[:, 0:3]
+        #print(df_common) working
+
+        #unique columns per allele
+        #1st allele
+        df_1st_allele = self.df.iloc[:, 3:9]
+
+        #2nd allele
+        df_2nd_allele = self.df.iloc[:, 9:15]
+
+        #3rd allele
+        df_3rd_allele = self.df.iloc[:, 15:21]
+
+        #4th allele
+        df_4th_allele = self.df.iloc[:, 21:27]
+
+        #5th allele
+        df_5th_allele = self.df.iloc[:, 27:33]
+
+        #new DataFrame object with all necessary from an allele
+        df_overall_1st = df_common.join(df_1st_allele)
+        df_overall_2nd = df_common.join(df_2nd_allele)
+        df_overall_3rd = df_common.join(df_3rd_allele)
+        df_overall_4th = df_common.join(df_4th_allele)
+        df_overall_5th = df_common.join(df_5th_allele)
+
+
+        #final csv file with 1st allele parsed
+        dff = df_overall_1st
+
+        #here the script selects the row containing the columns name
+        dff.columns = dff.iloc[0]
+        dff = dff.drop(0)
+
+        #as only analyses one peptide lenght i.e 9aa those columns are not necessary
+        dff = dff.drop(columns = ["core", "icore"])
+
+        #data type object changed in order to help to manipulate the data later on
+        dff["Pos"] = dff["Pos"].astype(int)
+        dff["EL_Rank"] = dff["EL_Rank"].astype(float)
+        dff["BA_Rank"] = dff["BA_Rank"].astype(float)
+
+        #columns of relevance selected and threshold recommended by NetMHCpan-4.1 developers
+        dff = dff[["Pos", "Peptide", "ID", "EL_Rank", "BA_Rank"]].query("EL_Rank <= 2")
+        dff = dff.sort_values(by=["EL_Rank"])
+
+        #saves the parsed data sorted by EL Rank% only at this moment
+        dff = dff.reset_index(drop=True)
+        dff.to_csv(f"{alleles_list[0]}.csv", index=False)
+
+
+        #final csv file with 2nd allele parsed
+        dff = df_overall_2nd
+
+        #here the script selects the row containing the columns name
+        dff.columns = dff.iloc[0]
+        dff = dff.drop(0)
+
+        #as only analyses one peptide lenght i.e 9aa those columns are not necessary
+        dff = dff.drop(columns = ["core", "icore"])
+
+        #data type object changed in order to help to manipulate the data later on
+        dff["Pos"] = dff["Pos"].astype(int)
+        dff["EL_Rank"] = dff["EL_Rank"].astype(float)
+        dff["BA_Rank"] = dff["BA_Rank"].astype(float)
+
+        #columns of relevance selected and threshold recommended by NetMHCpan-4.1 developers
+        dff = dff[["Pos", "Peptide", "ID", "EL_Rank", "BA_Rank"]].query("EL_Rank <= 2")
+        dff = dff.sort_values(by=["EL_Rank"])
+
+        #saves the parsed data sorted by EL Rank% only at this moment
+        dff = dff.reset_index(drop=True)
+        dff.to_csv(f"{alleles_list[1]}.csv", index=False)
+
+
+        #final csv file with 3rd allele parsed
+        dff = df_overall_3rd
+
+        #here the script selects the row containing the columns name
+        dff.columns = dff.iloc[0]
+        dff = dff.drop(0)
+
+        #as only analyses one peptide lenght i.e 9aa those columns are not necessary
+        dff = dff.drop(columns = ["core", "icore"])
+
+        #data type object changed in order to help to manipulate the data later on
+        dff["Pos"] = dff["Pos"].astype(int)
+        dff["EL_Rank"] = dff["EL_Rank"].astype(float)
+        dff["BA_Rank"] = dff["BA_Rank"].astype(float)
+
+        #columns of relevance selected and threshold recommended by NetMHCpan-4.1 developers
+        dff = dff[["Pos", "Peptide", "ID", "EL_Rank", "BA_Rank"]].query("EL_Rank <= 2")
+        dff = dff.sort_values(by=["EL_Rank"])
+
+        #saves the parsed data sorted by EL Rank% only at this moment
+        dff = dff.reset_index(drop=True)
+        dff.to_csv(f"{alleles_list[2]}.csv", index=False)
+
+
+        #final csv file with 4th allele parsed
+        dff = df_overall_4th
+        #here the script selects the row containing the columns name
+        dff.columns = dff.iloc[0]
+        dff = dff.drop(0)
+
+        #as only analyses one peptide lenght i.e 9aa those columns are not necessary
+        dff = dff.drop(columns = ["core", "icore"])
+
+        #data type object changed in order to help to manipulate the data later on
+        dff["Pos"] = dff["Pos"].astype(int)
+        dff["EL_Rank"] = dff["EL_Rank"].astype(float)
+        dff["BA_Rank"] = dff["BA_Rank"].astype(float)
+
+        #columns of relevance selected and threshold recommended by NetMHCpan-4.1 developers
+        dff = dff[["Pos", "Peptide", "ID", "EL_Rank", "BA_Rank"]].query("EL_Rank <= 2")
+        dff = dff.sort_values(by=["EL_Rank"])
+
+        #saves the parsed data sorted by EL Rank% only at this moment
+        dff = dff.reset_index(drop=True)
+        dff.to_csv(f"{alleles_list[3]}.csv", index=False)
+
+
+        #only to use if there is more than 5 alleles
+        #final csv file with 5th allele parsed
+        #dff = df_overall_5th
+        #here the script selects the row containing the columns name
+        #dff.columns = dff.iloc[0]
+        #dff = dff.drop(0)
+
+        #as only analyses one peptide lenght i.e 9aa those columns are not necessary
+        #dff = dff.drop(columns = ["core", "icore"])
+
+        #data type object changed in order to help to manipulate the data later on
+        #dff["Pos"] = dff["Pos"].astype(int)
+        #dff["EL_Rank"] = dff["EL_Rank"].astype(float)
+        #dff["BA_Rank"] = dff["BA_Rank"].astype(float)
+
+        #columns of relevance selected and threshold recommended by NetMHCpan-4.1 developers
+        #dff = dff[["Pos", "Peptide", "ID", "EL_Rank", "BA_Rank"]].query("EL_Rank <= 2")
+        #dff = dff.sort_values(by=["EL_Rank"])
+
+        #saves the parsed data sorted by EL Rank% only at this moment
+        #dff = dff.reset_index(drop=True)
+        #dff.to_csv(f"{alleles_list[4]}.csv", index=False)
 
 #*************************************************************************
 
